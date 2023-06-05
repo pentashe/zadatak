a9d0ac07018e428316d30dec5656a30570b5d1ae
Ivo Bilic-Subasic <ib52250@fer.hr>
2023-06-06 01:07:51 +0200

    Revert "data race fixed"
    This reverts commit be129982a1d9a5896081df0a32106801009cba26.

--- main.go
+++ main.go
@@ -123,7 +123,7 @@ func main() {
 	mux.HandleFunc("/0246096698", writeDataAdela)
 	mux.HandleFunc("/0036522500", writeDataIvo)
 
-	saxpy.Init()
+	go saxpy.Init()
 	mux.HandleFunc("/saxpy", saxpy.Handler)
 
 	srv := &http.Server{
